5e50b9630834a07da425eba2e7d26434946ff6df
Ftoy <spit10000@yahoo.gr>
2025-02-21 18:35:11 +0200

    Flash attn disable in samplle

--- lumina_next_t2i_mini/scripts/sample.sh
+++ lumina_next_t2i_mini/scripts/sample.sh
@@ -3,15 +3,15 @@
 
 # Lumina-Next supports any resolution (up to 2K)
 # res="1024:024x1024 1536:1536x1536 1664:1664x1664 1792:1792x1792 2048:2048x2048"
-res=1024:1024x1024
+res=512:512x512
 t=4
 cfg=4.0
-seed=25
+seed=69
 steps=20
 solver=midpoint
-model_dir=your/model/dir/here
+model_dir=Lumina-Next-SFT/consolidated.00-of-01.safetensors
 cap_dir=your/caption/dir/here
-out_dir=your/output/dir/here
+out_dir=output
 python -u sample.py --ckpt ${model_dir} \
 --image_save_path ${out_dir} \
 --solver ${solver} --num_sampling_steps ${steps} \
