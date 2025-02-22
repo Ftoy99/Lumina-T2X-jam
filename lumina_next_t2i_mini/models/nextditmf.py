29b42bdd362de283824841783ad98ec896b26069
Ftoy <spit10000@yahoo.gr>
2025-02-22 12:00:46 +0200

    Adjust models.package

--- lumina_next_t2i_mini/models/nextditmf.py
+++ lumina_next_t2i_mini/models/nextditmf.py
@@ -886,11 +886,12 @@ class NextDiT(nn.Module):
         combined = torch.cat([half, half], dim=0)
         combinedmf = torch.cat([halfmf, halfmf], dim=0)
         model_out = self(combined, combinedmf, t, cap_feats, cap_mask)
+        model_out_x,model_out_xmf = model_out
         # For exact reproducibility reasons, we apply classifier-free guidance on only
         # three channels by default. The standard approach to cfg applies it to all channels.
         # This can be done by uncommenting the following line and commenting-out the line following that.
         # eps, rest = model_out[:, :self.in_channels], model_out[:, self.in_channels:]
-        eps, rest = model_out[:, :3], model_out[:, 3:]
+        eps, rest = model_out_x[:, :3], model_out_x[:, 3:]
         cond_eps, uncond_eps = torch.split(eps, len(eps) // 2, dim=0)
         half_eps = uncond_eps + cfg_scale * (cond_eps - uncond_eps)
         eps = torch.cat([half_eps, half_eps], dim=0)
