cacb8349a270ad525443aa5d78d738ba5fa89fb2
Ftoy <spit10000@yahoo.gr>
2025-02-22 11:54:33 +0200

    Adjust models.package

--- lumina_next_t2i_mini/models/nextditmf.py
+++ lumina_next_t2i_mini/models/nextditmf.py
@@ -882,8 +882,10 @@ class NextDiT(nn.Module):
                 layer.attention.proportional_attn = proportional_attn
 
         half = x[: len(x) // 2]
+        halfmf = xmf[: len(x) // 2]
         combined = torch.cat([half, half], dim=0)
-        model_out = self(combined, t, cap_feats, cap_mask)
+        combinedmf = torch.cat([halfmf, halfmf], dim=0)
+        model_out = self(combined, combinedmf, t, cap_feats, cap_mask)
         # For exact reproducibility reasons, we apply classifier-free guidance on only
         # three channels by default. The standard approach to cfg applies it to all channels.
         # This can be done by uncommenting the following line and commenting-out the line following that.
