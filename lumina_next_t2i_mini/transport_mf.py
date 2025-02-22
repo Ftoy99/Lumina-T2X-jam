f5854e8f5f217831969e77807ab0d8694bcffb98
Ftoy <spit10000@yahoo.gr>
2025-02-22 17:45:57 +0200

    midpoint instead of euler

--- lumina_next_t2i_mini/transport_mf.py
+++ lumina_next_t2i_mini/transport_mf.py
@@ -89,7 +89,7 @@ class ODE:
         def _fn(t, x, xmf):
             t = th.ones(x[0].size(0)).to(device) * t if isinstance(x, tuple) else th.ones(x.size(0)).to(device) * t
             model_output, model_output_xmf = model(x, xmf, t, **model_kwargs)
-            return model_output,model_output_xmf
+            return model_output
 
         t = self.t.to(device)
         # samples = odeint(_fn, x, t, method=self.sampler_type)
@@ -98,19 +98,7 @@ class ODE:
         # sol = odeint(f, x0, t, method='euler', args=(omega,))
         # sol = odeint(lambda y, t: f(y, t, omega), x0, t, method='euler')
         # Samples
-        # samples_x,model_output_xmf = odeint(lambda y, t: _fn(y, t, xmf), x, t, method=self.sampler_type)  # holy shit # piece of fucking shit fuck you
+        samples_x = odeint(lambda y, t: _fn(y, t, xmf), x, t, method=self.sampler_type)  # holy shit
 
-        # Initialize the state
-        samples_x = x
-        samples_xmf = xmf
-
-        # Use Euler's method (or RK4, etc.) for integration
-        for i in range(len(t) - 1):
-            dt = t[i + 1] - t[i]
-            model_output, model_output_xmf = _fn(t[i], samples_x, samples_xmf)
-
-            # Update the state using the model outputs
-            samples_x = samples_x + model_output * dt  # Euler update
-            samples_xmf = samples_xmf + model_output_xmf * dt  # Euler update for xmf
-
+        print(f"samples_x shape {samples_x.shape}")
         return samples_x
