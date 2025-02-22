43b41bca6b74b329855a61c0d1a5df0fa216f8f0
Ftoy <spit10000@yahoo.gr>
2025-02-22 11:17:33 +0200

    Adjust models.package

--- lumina_next_t2i_mini/models/__init__.py
+++ lumina_next_t2i_mini/models/__init__.py
@@ -1,2 +1,2 @@
 from .nextditmf import NextDiT_2B_GQA_patch2_mf, NextDiT_2B_patch2_mf
-from .nextdit import NextDiT_2B_GQA_patch2, NextDiT_2B_patch2
+# from .nextdit import NextDiT_2B_GQA_patch2, NextDiT_2B_patch2 # No flash attn
