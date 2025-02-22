92bee8ba45dd084ecc881a72a8e74fc2e2efbcc3
Ftoy <spit10000@yahoo.gr>
2025-02-22 13:46:08 +0200

    seperate of transport

--- lumina_next_t2i_mini/transport.py
+++ lumina_next_t2i_mini/transport.py
@@ -88,6 +88,9 @@ class ODE:
         if not self.use_sd3:
 
             def _fn(t, x):
+                print(t)
+                print(x)
+                print(x.shape)
                 t = th.ones(x[0].size(0)).to(device) * t if isinstance(x, tuple) else th.ones(x.size(0)).to(device) * t
                 model_output = model(x, t, **model_kwargs)
                 return model_output
