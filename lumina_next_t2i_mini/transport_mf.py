8e800e8bec3c463f04a81f0f5922ef0242c2dac6
Ftoy <spit10000@yahoo.gr>
2025-02-22 15:03:13 +0200

    Revert stuff

--- lumina_next_t2i_mini/transport_mf.py
+++ lumina_next_t2i_mini/transport_mf.py
@@ -86,8 +86,8 @@ class ODE:
         device = x[0].device if isinstance(x, tuple) else x.device
 
         if not self.use_sd3:
-
-            def _fn(t, x):
+            def _fn(t, x_comb):
+                x, xmf = x_comb
                 t = th.ones(x[0].size(0)).to(device) * t if isinstance(x, tuple) else th.ones(x.size(0)).to(
                     device) * t
                 model_output = model(x, t, **model_kwargs)
@@ -97,11 +97,11 @@ class ODE:
             cfg_scale = model_kwargs["cfg_scale"]
             model_kwargs.pop("cfg_scale")
 
-            def _fn(t, x):
+            def _fn(t, x_comb):
+                x, xmf = x_comb
                 t = th.ones(x.size(0)).to(device) * t * 1000
                 half_x = x[: len(x) // 2]
                 x = th.cat([half_x, half_x], dim=0)
-                print(f"Forward 2")
                 model_output = model(hidden_states=x, timestep=t, **model_kwargs)[0]
                 uncond, cond = model_output.chunk(2, dim=0)
                 model_output = uncond + cfg_scale * (cond - uncond)
@@ -109,5 +109,6 @@ class ODE:
                 return model_output
 
         t = self.t.to(device)
-        samples = odeint(_fn, x, t, method=self.sampler_type)
+        xcomb = (x, xmf)
+        samples = odeint(_fn, xcomb, t, method=self.sampler_type)
         return samples
