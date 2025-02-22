c1d41bb10cbf36ac0a28d73eabcf00f71dd5788e
Ftoy <spit10000@yahoo.gr>
2025-02-22 15:23:35 +0200

    Debug odeint

--- lumina_next_t2i_mini/models/nextdit.py
+++ lumina_next_t2i_mini/models/nextdit.py
@@ -825,6 +825,7 @@ class NextDiT(nn.Module):
         half = x[: len(x) // 2]
         combined = torch.cat([half, half], dim=0)
         model_out = self(combined, t, cap_feats, cap_mask)
+        print(model_out)
         # For exact reproducibility reasons, we apply classifier-free guidance on only
         # three channels by default. The standard approach to cfg applies it to all channels.
         # This can be done by uncommenting the following line and commenting-out the line following that.
