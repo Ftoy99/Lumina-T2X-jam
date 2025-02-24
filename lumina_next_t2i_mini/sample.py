2411c29ab810edfe7ccee40397f17dfa4a5f5fb4
Ftoy <spit10000@yahoo.gr>
2025-02-24 09:06:21 +0200

    Init weights

--- lumina_next_t2i_mini/sample.py
+++ lumina_next_t2i_mini/sample.py
@@ -124,15 +124,6 @@ def main(args, rank, master_port):
         )
         model.load_state_dict(ckpt, strict=True)
 
-    with torch.no_grad():
-        model.x_cat_emb.weight[:, :model.x_embedder.in_features] = model.x_embedder.weight  # Copy pre-trained weights
-        print(f"x_cat_emb.shape {model.x_cat_emb.weight[:, :model.x_embedder.in_features].shape} xemb.shape {model.x_embedder.weight.shape}")
-        model.x_cat_emb.bias.copy_(model.x_embedder.bias)  # Copy bias
-        print(f"xcat.bias.shape {model.x_cat_emb.bias.shape} model.x_embedder.bias.shape {model.model.x_embedder.bias.shape}")
-        # Zero out the motion-related part (newly added rows)
-        motion_dim_start = model.x_embedder.in_features
-        model.x_cat_emb.weight[:, motion_dim_start:].zero_()
-
     sample_folder_dir = args.image_save_path
 
     if rank == 0:
