9e4b11e9a3d3edfe042a1edee75cf4439fa9ec6d
Ftoy <spit10000@yahoo.gr>
2025-02-28 12:51:23 +0200

    Remove temp decode

--- lumina_next_t2i_mini/vae.py
+++ lumina_next_t2i_mini/vae.py
@@ -35,7 +35,8 @@ def encode_frames(frames):
         frames_resized = np.array([cv2.resize(frame, (512, 512)) for frame in frames])  # Resize all frames
         print(f"np array frames shape {frames_resized.shape}")  # np array frames shape (708, 512, 512, 3)
         #  batch_size, num_channels, num_frames, height, width = x.shape
-        frames_tensor = torch.tensor(frames_resized).permute(3, 0, 1, 2).unsqueeze(0)  # Convert to (Frames, Channels, H, W)
+        frames_tensor = torch.tensor(frames_resized).permute(3, 0, 1, 2).unsqueeze(
+            0)  # Convert to (Frames, Channels, H, W)
         frames_tensor = frames_tensor.to(torch.float16).to("cuda") / 127.5 - 1  # Normalize
         print(f"frames_tensor shape {frames_tensor.shape}")
         latent = vae.encode(frames_tensor).latent_dist.sample()
@@ -43,17 +44,15 @@ def encode_frames(frames):
 
 
 def decode_frames(latents):
-    """Decode latents back to video frames."""
-    frames = []
     with torch.no_grad():
-            decoded = vae.decode(latents).sample
-            print(f"Decoded shapes {decoded.shape}")
-            #decoded_image = ((decoded.squeeze(0).permute(1, 2, 0).cpu().float() + 1) * 127.5).clamp(0,255).byte().numpy()
-            frames.append(decoded)
-    return frames
+        decoded = vae.decode(latents).sample
+        print(f"Decoded shapes {decoded.shape}")
+        decoded_video = ((decoded.squeeze(0).permute(1, 2, 3, 0).cpu().float() + 1) * 127.5).clamp(0,
+                                                                                                   255).byte().numpy()
+        return decoded_video
 
 
-def save_video(frames, output_path, fps=30):
+def save_video(decoded_video, output_path, fps=30):
     """Save frames as a video."""
     height, width, _ = frames[0].shape
     fourcc = cv2.VideoWriter_fourcc(*"mp4v")  # Codec for MP4
@@ -79,7 +78,7 @@ print("Encoding complete.")
 
 print(f"Latents shape {latents.shape}")
 
-#Decode frames
+# Decode frames
 decoded_frames = decode_frames(latents)
 print("Decoding complete.")
 #
