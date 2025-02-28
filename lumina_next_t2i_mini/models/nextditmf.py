c457a46b916df0b8b12d7c934af83b05c6653786
Ftoy <spit10000@yahoo.gr>
2025-02-28 16:09:58 +0200

    Concat same dimension fix

--- lumina_next_t2i_mini/models/nextditmf.py
+++ lumina_next_t2i_mini/models/nextditmf.py
@@ -663,7 +663,7 @@ class NextDiT(nn.Module):
         assert (dim // n_heads) % 4 == 0, "2d rope needs head dim to be divisible by 4"
         self.freqs_cis = NextDiT.precompute_freqs_cis(
             #dim // n_heads,
-            51200,
+            51200// n_heads,
             384,
             scale_factor=scale_factor,
         )
