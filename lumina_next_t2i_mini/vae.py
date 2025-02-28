40b8c8f61426c8e9d0f26f7753bd863d10b97fcb
Ftoy <spit10000@yahoo.gr>
2025-02-28 11:20:34 +0200

    Frames tensor

--- lumina_next_t2i_mini/vae.py
+++ lumina_next_t2i_mini/vae.py
@@ -34,8 +34,9 @@ def encode_frames(frames):
     """Encode frames to latent space."""
     latents = []
     with torch.no_grad():
-        frames_resized = [cv2.resize(frame, (512, 512)) for frame in frames]  # Resize all frames
-        frames_tensor = torch.tensor(np.array(frames_resized)).permute(0, 3, 1, 2)  # Convert to (B, C, H, W)
+        frames_resized = np.array([cv2.resize(frame, (512, 512)) for frame in frames])  # Resize all frames
+        print(f"np array frames shape {frames_resized.shape}")
+        frames_tensor = torch.tensor(frames_resized).permute(0, 3, 1, 2)  # Convert to (B, C, H, W)
         frames_tensor = frames_tensor.to(torch.float16).to("cuda") / 127.5 - 1  # Normalize
         print(f"frames_tensor shape {frames_tensor.shape}")
         latent = pipe.encode(frames_tensor).latent_dist.sample()
