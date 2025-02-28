259f2d2caa83e6f81c4b21d13cfad3067badadf5
Ftoy <spit10000@yahoo.gr>
2025-02-28 16:12:43 +0200

    Concat same dimension fix

--- lumina_next_t2i_mini/sample_mf.py
+++ lumina_next_t2i_mini/sample_mf.py
@@ -187,7 +187,7 @@ def main(args, rank, master_port):
                 latent_w, latent_h = w // 8, h // 8
 
                 # Latent picture noise
-                z = torch.randn([1, 4, 50, latent_w, latent_h], device="cuda").to(dtype)
+                z = torch.randn([1, 4, 10, latent_w, latent_h], device="cuda").to(dtype)
                 z = z.repeat(n * 2, 1, 1, 1, 1)
 
                 print(f"z.shape {z.shape}")
