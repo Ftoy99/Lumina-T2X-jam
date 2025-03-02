5efe9bd9998f21d4fb6080f29257fdf013b1f575
Ftoy <spit10000@yahoo.gr>
2025-03-02 10:50:54 +0200

    Training script

--- lumina_next_t2i_mini/train_vae_swap_layers.py
+++ lumina_next_t2i_mini/train_vae_swap_layers.py
@@ -106,7 +106,7 @@ def main(args):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("--first_run", type=int, default=False)
+    parser.add_argument("--first_run", type=bool, default=False)
 
     args = parser.parse_known_args()[0]
     main(args)
