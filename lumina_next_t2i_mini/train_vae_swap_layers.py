b3c906d589683b6b681867b3796c665f9a4fb844
Ftoy <spit10000@yahoo.gr>
2025-03-03 11:13:13 +0200

    Device encode prompts debug

--- lumina_next_t2i_mini/train_vae_swap_layers.py
+++ lumina_next_t2i_mini/train_vae_swap_layers.py
@@ -96,6 +96,9 @@ def encode_prompt(prompt_batch, text_encoder, tokenizer, proportion_empty_prompt
         text_input_ids = text_inputs.input_ids
         prompt_masks = text_inputs.attention_mask
 
+        print(text_input_ids.device)
+        print(prompt_masks.device)
+        print(text_encoder.device)
         prompt_embeds = text_encoder(
             input_ids=text_input_ids,
             attention_mask=prompt_masks,
