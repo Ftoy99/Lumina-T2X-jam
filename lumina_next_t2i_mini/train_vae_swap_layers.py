c972df4525aa7231f8cc53a69d075984a9a18342
Ftoy <spit10000@yahoo.gr>
2025-03-03 14:22:30 +0200

    Debug x_maks shape attn

--- lumina_next_t2i_mini/train_vae_swap_layers.py
+++ lumina_next_t2i_mini/train_vae_swap_layers.py
@@ -228,7 +228,7 @@ def main(args):
         opt.zero_grad()
         model_kwargs = dict(cap_feats=cap_feats, cap_mask=cap_mask)
         with torch.cuda.amp.autocast(dtype=torch.float16):
-            latent = latent.repeat(2, 1, 5, 1, 1)
+            latent = latent.repeat(2, 1, 1, 1, 1)
             loss_dict = training_losses(model, latent, latent, model_kwargs)
             loss = loss_dict["loss"].sum()
             loss_item += loss.item()
