b7721e2609c49861c10831c6756d16b341922452
Ftoy <spit10000@yahoo.gr>
2025-03-03 16:53:45 +0200

    Loss debug

--- lumina_next_t2i_mini/models/nextditmf.py
+++ lumina_next_t2i_mini/models/nextditmf.py
@@ -864,6 +864,7 @@ class NextDiT(nn.Module):
         cap_emb = self.cap_embedder(cap_feats_pool)
         adaln_input = t + cap_emb
 
+        cap_mask = cap_mask.bool()
         for idx, layer in enumerate(self.layers):
             print(f"Running layer {idx}")
             x = layer(x, mask, freqs_cis, cap_feats, cap_mask, adaln_input=adaln_input)
