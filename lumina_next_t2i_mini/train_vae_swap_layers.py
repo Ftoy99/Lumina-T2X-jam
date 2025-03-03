7a776c3c94866e74cd15a733b32eaf7aa5ef72a2
Ftoy <spit10000@yahoo.gr>
2025-03-03 17:02:34 +0200

    Remove autocast

--- lumina_next_t2i_mini/train_vae_swap_layers.py
+++ lumina_next_t2i_mini/train_vae_swap_layers.py
@@ -243,9 +243,8 @@ def main(args):
         loss_item = 0.0
         opt.zero_grad()
         model_kwargs = dict(cap_feats=cap_feats, cap_mask=cap_mask)
-        with torch.cuda.amp.autocast(dtype=torch.float16,enabled=False):
+        with torch.cuda.amp.autocast(dtype=torch.float16):
             # latent = latent.repeat(2, 1, 1, 1, 1)
-            latent = latent.to(torch.float32)
             loss_dict = training_losses(model, latent, latent, model_kwargs)
             logger.info(f"loss dict {loss_dict}")
             loss = loss_dict["loss"].sum()
