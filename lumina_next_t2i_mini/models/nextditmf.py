b655690f4a25b3c7f45497a7cd879413b06965e5
Ftoy <spit10000@yahoo.gr>
2025-03-03 15:12:26 +0200

    Remove prints

--- lumina_next_t2i_mini/models/nextditmf.py
+++ lumina_next_t2i_mini/models/nextditmf.py
@@ -867,10 +867,11 @@ class NextDiT(nn.Module):
             x = layer(x, mask, freqs_cis, cap_feats, cap_mask, adaln_input=adaln_input)
 
         # TODO PASS FRAMES TO MODEL
-        frames_size = 5
+
         x_out = self.final_layer(x, adaln_input)
         xmf_out = self.final_layer_xmf(x, adaln_input)
 
+        frames_size = 5
         x_out = self.unpatchify(x_out, img_size, frames_size, return_tensor=x_is_tensor)
         xmf_out = self.unpatchify(xmf_out, img_size, frames_size, return_tensor=x_is_tensor)
 
