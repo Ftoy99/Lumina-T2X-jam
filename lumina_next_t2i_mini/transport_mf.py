b97e5b0d64f30781b370f540a1905816cf9e86d6
Ftoy <spit10000@yahoo.gr>
2025-03-03 20:26:08 +0200

    Checkpointing

--- lumina_next_t2i_mini/transport_mf.py
+++ lumina_next_t2i_mini/transport_mf.py
@@ -1,12 +1,6 @@
 import torch as th
 from torchdiffeq import odeint
 
-from torch.utils.checkpoint import checkpoint
-
-
-def checkpointed_forward(model, xt, mft, t, **model_kwargs):
-    return model(xt, mft, t, **model_kwargs)
-
 
 def sample(x1, mf1):
     """Sampling x0 & t based on shape of x1 (if needed)
@@ -55,8 +49,7 @@ def training_losses(model, x1, mf1, model_kwargs=None):
         mft = t_ * mf1 + (1 - t_) * mf0
         mfut = mf1 - mf0
 
-    # Use checkpointing here to save memory during the forward pass
-    model_output, model_output_mf = checkpoint(checkpointed_forward, model, xt, mft, t, **model_kwargs)
+    model_output, model_output_mf = model(xt, mft, t, **model_kwargs)
 
     terms = {}
 
