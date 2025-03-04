dfae9226ec0d15bf8e09b1cc0edc5188df2b00da
Ftoy <spit10000@yahoo.gr>
2025-03-04 15:51:11 +0200

    Samplel vae trained

--- lumina_next_t2i_mini/train_vae_swap_layers.py
+++ lumina_next_t2i_mini/train_vae_swap_layers.py
@@ -288,7 +288,7 @@ def main(args):
         loss_item += loss.item()
         # Save the model every 200 steps (or adjust as needed)
         if (step + 1) % 200 == 0:
-            save_file(model.state_dict(), f'custom_ckpt/vae_trained_layers.safetensors')
+            save_file(model.state_dict(), f'custom_ckpt/1.safetensors')
             logger.info(model.state_dict())
             logger.info(f"Saved model checkpoint at steps {step+1}")
 
