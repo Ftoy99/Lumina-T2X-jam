570f26a2bd29f3c9b4e911313e9bcebd81674c00
Ftoy <spit10000@yahoo.gr>
2025-03-05 18:12:22 +0200

    Revert V2 Freq

--- lumina_next_t2i_mini/samplev2.py
+++ lumina_next_t2i_mini/samplev2.py
@@ -181,11 +181,11 @@ def main(args, rank, master_port):
                 latent_w, latent_h = w // 8, h // 8
 
                 # Latent
-                z = torch.randn([1, 4, 1, latent_w, latent_h], device="cuda").to(dtype)
+                z = torch.randn([1, 4, 2, latent_w, latent_h], device="cuda").to(dtype)
                 z = z.repeat(n * 2, 1, 1, 1, 1)
 
                 # Latent Motion Noise
-                zmf = torch.randn([1, 4, 1, latent_w, latent_h], device="cuda").to(dtype)
+                zmf = torch.randn([1, 4, 2, latent_w, latent_h], device="cuda").to(dtype)
                 zmf = zmf.repeat(n * 2, 1, 1, 1, 1)
 
                 with torch.no_grad():
